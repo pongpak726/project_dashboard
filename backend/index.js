d276a81e52d595ce3880eec4fc466afde7c40e24
pongpak726 <fuza2560@gmail.com>
2026-04-09 15:42:00 +0700

    feat(server): setup server entry point

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app = require("./server");
+require("dotenv").config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
