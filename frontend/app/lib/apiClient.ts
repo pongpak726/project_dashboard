0737d0d241f07f5dae9b7ab45dc2656879729e30
pongpak726 <fuza2560@gmail.com>
2026-04-16 13:47:10 +0700

    fix(apiClient): correct syntax error

--- frontend/app/lib/apiClient.ts
+++ frontend/app/lib/apiClient.ts
@@ -2,7 +2,7 @@ export const apiClient = async (url: string) => {
     const token = localStorage.getItem("token")
     const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`;
 
-    const res = await fetch(`${API_URL} + url`, {
+    const res = await fetch(`${API_URL}${url}`, {
         headers: {
             Authorization: `Bearer ${token}`
         }
