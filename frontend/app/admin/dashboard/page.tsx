8856297af341f7429d60436d7f32020ad475d379
pongpak726 <fuza2560@gmail.com>
2026-04-17 12:49:07 +0700

    style(ui): improve admin dashboard styling

--- frontend/app/admin/dashboard/page.tsx
+++ frontend/app/admin/dashboard/page.tsx
@@ -1,7 +1,9 @@
 export default function AdminDashboardPage() {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
+    <>
+        <div className="bg-gray-100 p-6 text-black min-h-full">
+            <h1 className="text-black text-center font-bold text-2xl pt-2">Admin Dashboard</h1>
+        </div>
+    </>
   )
 }
