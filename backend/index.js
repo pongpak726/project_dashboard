c2ca22422356f5383bca5f9c44fcc4ea98213255
pongpak726 <fuza2560@gmail.com>
2026-04-20 13:57:31 +0700

    feat(server): add external API

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-const app = require("./server");
 require("dotenv").config();
+const app = require("./server");
+
 
 const PORT = process.env.PORT || 5000;
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,29 @@ app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
+// external API
+app.get('/api/external', async (req, res, next) => {
+  try {
+    const response = await fetch(process.env.EXTERNAL_API, {
+      headers: {
+        Authorization: `Bearer ${process.env.SECRET_API}`,
+        "Content-Type": "application/json"
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`External API error: ${response.status}`)
+    }
+
+    const data = await response.json()
+
+    res.json(data)
+  } catch (err) {
+    next(err) // ส่งไป errorHandler
+  }
+})
+
+
 app.use(errorHandler);
 
 module.exports = app;
