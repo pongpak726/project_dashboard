dca17ea7ee15da0774c37ed4757887d25cb5dfe4
pongpak726 <fuza2560@gmail.com>
2026-04-20 17:10:13 +0700

    feat(api): add restroom external API

--- backend/controllers/external.controller.js
+++ backend/controllers/external.controller.js
@@ -2,7 +2,27 @@ const externalService = require("../services/external.service")
 
 exports.getWeather = async (req, res, next) => {
   try {
-    const data = await externalService.getWeather()
+    const { site, limit } = req.query
+
+    const data = await externalService.getWeather({
+      site,
+      limit
+    })
+
+    res.json({ success: true, data })
+  } catch (err) {
+    next(err)
+  }
+}
+
+exports.getRestroom = async (req, res, next) => {
+  try {
+    const { site, limit } = req.query
+
+    const data = await externalService.getRestroom({
+      site,
+      limit
+    })
 
     res.json({
       success: true,
--- backend/routes/external.routes.js
+++ backend/routes/external.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controllers/external.controller")
 
 router.get("/weather", controller.getWeather)
+router.get("/restroom", controller.getRestroom)
 
 module.exports = router
